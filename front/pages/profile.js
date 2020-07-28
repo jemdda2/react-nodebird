f0b15452c0cd7bc68d04f05139519253eec54695
金 池淳 <siriusda2@naver.com>
2020-07-28 18:54:34 +0900

    20200728 SWRを使って実装

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,36 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Head from 'next/head';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Router from 'next/router';
 import { END } from 'redux-saga';
 import axios from 'axios';
+import useSWR from 'swr';
 
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameeEditForm';
 import FollowList from '../components/FollowList';
-import { LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST } from '../reducers/user';
+import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
+import { LOAD_POSTS_REQUEST } from '../reducers/post';
 import wrapper from '../store/configureStore';
 
-const Profile = () => {
-  const dispatch = useDispatch();
-  const { me } = useSelector((state) => state.user);
+const fetcher = (url) => axios.get(url, {withCredentials: true}).then((result) => result.data);
 
-  useEffect(() => {
-    dispatch({
-      type: LOAD_FOLLOWERS_REQUEST,
-    });
-    dispatch({
-      type: LOAD_FOLLOWINGS_REQUEST,
-    });
-  }, []);
+const Profile = () => {
+  const { me } = useSelector((state) => state.user);
+  const [followersLimit, setFollowersLimit] = useState(3);
+  const [followingsLimit, setFollowingsLimit] = useState(3);
+  
+  const { data: followersData , error: followerError } = useSWR(`http://localhost:3065/user/followers?limit=${followersLimit}`, fetcher)
+  const { data: followingsData, error: followingError } = useSWR(`http://localhost:3065/user/followings?limit=${followingsLimit}`, fetcher)
 
   useEffect(() => {
     if (!(me && me.id)) {
       Router.push('/');
     }
   }, [me && me.id]);
+
   if (!me) {
-    return null;
+    return 'ログイン情報をロード中。。。';
+  }
+
+  if (followerError || followingError) {
+    console.error(followerError || followingError);
+    return <div>follower/followingロード中にエラーが発生しました。</div>;
   }
 
   return (
@@ -40,8 +45,8 @@ const Profile = () => {
       </Head>
       <AppLayout>
         <NicknameEditForm />
-        <FollowList header="FOLLOWING" data={me.Followings} />
-        <FollowList header="FOLLOWER" data={me.Followers} />
+        <FollowList header="FOLLOWING" data={followingsData} />
+        <FollowList header="FOLLOWER" data={followersData} />
       </AppLayout>
     </>
   );
